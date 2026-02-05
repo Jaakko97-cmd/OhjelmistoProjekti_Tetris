68a5edc13d00eab637a5497ae52296e7cf1d1927
Tuike91 <tuuulia91@gmail.com>
2026-02-05 20:34:49 +0200

    peli-ikkuna ja looppi

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import pygame
+import pygame, sys
+
+screen = pygame.display.set_mode((500, 620))
+pygame.display.set_caption("Tetris Overwhelmed")
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
