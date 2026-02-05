70e7a09c06c2800dd1185f5aae34439e146a96ce
Tuike91 <tuuulia91@gmail.com>
2026-02-05 22:19:15 +0200

    main/muokkaus(clock+update)

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import pygame, sys
 screen = pygame.display.set_mode((500, 620))
 pygame.display.set_caption("Tetris Overwhelmed")
 
+clock = pygame.time.Clock()
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+
+	pygame.display.update()
+	clock.tick(60)
